cfe2d9c4de7ab00944befe6089a5538fa33d045e
Jess Morrison <jessmorrison90@gmail.com>
2020-08-21 13:19:33 -0500

    Updates

---
+++ precache-manifest.27e659ca8fb5fd5b02050d07b0131384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c65111f7667b6e588da96b2a0d9bd3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "53f1881108cace9cdbff",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f1881108cace9cdbff",
+    "url": "/portfolio/static/js/main.3be64f28.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
