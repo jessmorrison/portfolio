a96ac58d0b02cb57889f01072a43e2caa085fd1f
Jess Morrison <jessmorrison90@gmail.com>
2020-08-22 01:52:53 -0500

    big font long lines

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div class="JM__title--treatment">JESS MORRISON</div>
+        <div class="JM__title--treatment__v1">JESS MORRISON</div>
         <div class="JM__animated--line__TT" />
         <div class="JM__animated--line__TT" />
         <div class="JM__animated--line__TT" />
         <div class="JM__animated--line__TT" />
-        <div class="JM__animated--line__TT" />
+        <div class="JM__animated--line__TT--Black" />
         <div class="JM__animated--line__TT" />
         <div class="JM__animated--line__TT" />
         <div class="JM__animated--line__TT" />
