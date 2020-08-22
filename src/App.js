3593b19de8db1158f24833284f78072b4afbcb0f
Jess Morrison <jessmorrison90@gmail.com>
2020-08-21 21:10:38 -0500

    wave 1 scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import './App.css';
+import './index.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div class="JM__title--treatment">Jess Morrison</div>
+        <div class="JM__title--treatment">JESS MORRISON</div>
         <div class="JM__animated--line__TT" />
+        <div class="JM__animated--line__TT" />
+        <div class="JM__animated--line__TT" />
+        <div class="JM__animated--line__TT" />
+        <div class="JM__animated--line__TT" />
+        <div class="JM__animated--line__TT" />
+        <div class="JM__animated--line__TT" />
+        <div class="JM__animated--line__TT" />
+        <div class="JM__animated--line__TT" />
+     
       </header>
     </div>
   );
